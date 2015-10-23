930c0bf95ba7b3382ea70cc6f212a70e5e6be313
joe3haha <joe3haha@gmail.com>
2015-10-22 21:22:02 -0500

    Added messages back
    Added the messages functionality back to the code.

--- app/src/main/java/alextorres/smsapp/SMS.java
+++ app/src/main/java/alextorres/smsapp/SMS.java
@@ -100,6 +100,10 @@ public class SMS extends Activity {
         startActivity(intent);
     }
 
+    public void toMessages(View view){
+        Intent intent = new Intent(this, SmsRecieve.class);
+        startActivity(intent);
+    }
 
     public void toContacts(View view){
         Intent intent = new Intent(this, ContactListActivity.class);
