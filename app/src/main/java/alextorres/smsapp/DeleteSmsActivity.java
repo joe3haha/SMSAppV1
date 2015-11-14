b4eb15bf11925dfdde146642928c49810fd4e511
falexTorres <rkc870@my.utsa.edu>
2015-11-13 19:42:26 -0600

    search feature
    search messages first try

--- app/src/main/java/alextorres/smsapp/DeleteSmsActivity.java
+++ app/src/main/java/alextorres/smsapp/DeleteSmsActivity.java
@@ -4,8 +4,6 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
